68be9736fa36f4d75fb2b0bd69ec0a4f311217e9
Ikpia <echukwuka97@gmail.com>
2024-10-30 15:16:35 +0100

    adding more core functionalities

--- src/IPManagement.sol
+++ src/IPManagement.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "../src/IPLicensing.sol";
 
-contract CreateIPNFT is ERC721, Ownable {
+contract CreateIPNFT is ERC721, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     struct CreativeWork {
         string title;
         string metadataURI;
@@ -14,13 +16,17 @@ contract CreateIPNFT is ERC721, Ownable {
     uint256 private _tokenIds;
     mapping(uint256 => CreativeWork) private _creativeWorks;
 
-    constructor() ERC721("IPOwnershipNFT", "IPONFT") Ownable(msg.sender) {}
+    constructor() ERC721("IPOwnershipNFT", "IPONFT") Ownable(msg.sender) {
+        // The default admin role which can manage other roles
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
 
     function mintCreativeWork(
         address recipient,
         string memory _title,
         string memory _metadataURI
-    ) public onlyOwner returns (uint256) {
+    ) public onlyRole(MINTER_ROLE) returns (uint256) {
         _tokenIds++;
         uint256 newItemId = _tokenIds;
 
@@ -30,12 +36,33 @@ contract CreateIPNFT is ERC721, Ownable {
         return newItemId;
     }
 
+    function burnCreativeWork(uint256 _tokenId) onlyRole(MINTER_ROLE) {
+        require(_exists(_tokenId), "Creative work does not exist");
+
+        delete _creativeWorks[_tokenId];
+        _burn(tokenId);
+    }
+
+    // Admin function to add minter
+    function addMinter(address _minter) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        grantRole(MINTER_ROLE, _minter);
+    }
+
+    // Admin function to remove minter
+    function revokeMinter(address _minter) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(MINTER_ROLE, _minter);
+    }
+
     function getCreativeWork(
         uint256 tokenId
     ) public view returns (string memory title, string memory metadataURI) {
         require(_exists(tokenId), "Creative work does not exist");
         CreativeWork memory work = _creativeWorks[tokenId];
         return (work.title, work.metadataURI);
+    }
+
+    function noOfMintedCreativeWork() public returns (uint256) {
+        return _tokenIds;
     }
 
     function _exists(uint256 tokenId) public view returns (bool) {
